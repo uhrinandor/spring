f26d1ceedf51616d9488a716bea28bf93c4820d7
Elmer Kolos <eekke1034@gmail.com>
2026-03-26 17:16:46 +0100

    ILayerVisitor

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/ILayerVisitor.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/ILayerVisitor.java
@@ -3,5 +3,7 @@ package com.spring.app.skeleton.models.layer;
 import com.spring.app.skeleton.utils.IEntity;
 
 public interface ILayerVisitor extends IEntity {
-    public void visit(ILayer layer);
+    public void visit(Snow s);
+    public void visit(Ice i);
+    public void visit(Layer l);
 }
