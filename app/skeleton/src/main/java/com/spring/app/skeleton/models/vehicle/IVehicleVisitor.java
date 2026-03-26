7aed809a5d9b991b01c32bbeb1355cc725c59a36
Elmer Kolos <eekke1034@gmail.com>
2026-03-26 17:14:13 +0100

    IVehicleVisitor

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/IVehicleVisitor.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/IVehicleVisitor.java
@@ -3,5 +3,7 @@ package com.spring.app.skeleton.models.vehicle;
 import com.spring.app.skeleton.utils.IEntity;
 
 public interface IVehicleVisitor extends IEntity {
-
+    public void visit(Car c);
+    public void visit(Snowplow s);
+    public void visit(Bus b);
 }
