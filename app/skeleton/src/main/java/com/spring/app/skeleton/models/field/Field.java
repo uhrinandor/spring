cc7be008322ead0235f52f133930b57b32a612f5
Elmer Kolos <eekke1034@gmail.com>
2026-03-26 16:37:52 +0100

    fix

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/field/Field.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/field/Field.java
@@ -1,6 +1,6 @@
 package com.spring.app.skeleton.models.field;
 
-import com.spring.app.skeleton.utils.Entity;
+import .spring.app.skeleton.utils.Entity;
 
 public class Field extends Entity implements IField{
     
--- app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/ISnowPlow.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/ISnowPlow.java
@@ -2,6 +2,6 @@ package com.spring.app.skeleton.models.vehicle;
 
 import com.spring.app.skeleton.utils.IEntity;
 
-public class ISnowPlow extends IEntity {
+public interface ISnowPlow extends IEntity {
 
 }
