9f4db8f56d68ffc34e583781f425a5bae47d60e6
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-03-26 20:51:38 +0100

    buildings

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/buildings/Building.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/buildings/Building.java
@@ -3,5 +3,14 @@ package com.spring.app.skeleton.models.buildings;
 import com.spring.app.skeleton.utils.Entity;
 
 public abstract class Building extends Entity{
+    protected IField field;
     
+    public Building(IField field)
+    {
+        this.field=field;
+    }
+
+    public IField getField() {
+        return field;
+    }
 }
--- app/skeleton/src/main/java/com/spring/app/skeleton/models/buildings/Station.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/buildings/Station.java
@@ -4,10 +4,28 @@ import java.util.List;
 
 public class Station extends Building{
 
+    private Station pair;
+    public Station(IField field)
+    {
+        this.field=field;
+    }
+
+    public Station getPair()
+    {
+        return pair;
+    }
+
+    public void put(Bus bus)
+    {
+        getField().setVehicle(bus);
+        bus.setStation(getField());
+    }
+
     @Override
     public List<String> init() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'init'");
+        
     }
 
 }
