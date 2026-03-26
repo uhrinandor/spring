c0d6e4fea767648a0a159d340e160987481e5995
Elmer Kolos <eekke1034@gmail.com>
2026-03-26 17:09:51 +0100

    ShopItem

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/shop/ShopItem.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/shop/ShopItem.java
@@ -3,5 +3,6 @@ package com.spring.app.skeleton.models.shop;
 import com.spring.app.skeleton.utils.IEntity;
 
 public interface ShopItem extends IEntity {
-
+    public int price();
+    public void apply(PurchaseContext ctx, int amount);
 }
