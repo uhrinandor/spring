881b28deb1df2af53fafbd58152514b10a6a30fe
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-03-27 16:31:44 +0100

    comments

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/head/Broom.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/head/Broom.java
@@ -12,6 +12,12 @@ import com.spring.app.skeleton.models.vehicle.ISnowPlow;
 import com.spring.app.skeleton.utils.Entity;
 import com.spring.app.skeleton.utils.Tracer;
 
+/**
+ * A hányófej felelőssége az, hogy a hókotróra szerelve az útról a havat vagy a feltört jeget
+ * a közvetlenül tőle jobbra található Fielddel szomszédos jobb oldali Field-re tolja,
+ * vagy ha nincs ilyen, akkor a hó vagy feltört jég az út szélére kerül
+ * ezek után a játék során tovább nem foglalkozunk velük.
+ */
 public class Broom extends Entity implements IHead, ShopItem{
     
     @Override
@@ -19,8 +25,17 @@ public class Broom extends Entity implements IHead, ShopItem{
         return List.of();
     }
 
+    /**
+     * Ez a metódus felel a hókotró és a mezőn lévő réteg interakciójának kezeléséért.
+     * A havat és a tört jeget eltakarítja kettővel jobbra lévő útra vagy ha nincs ilyen
+     * akkor eltűnteti. A sima jégre nem tud hatással lenni.
+     * @param field amit le kéne takarítani
+     * @return ha a visitor szerint nem takaríthatja le a mezőt, hamissal tér vissza,
+     * ha letakarította igazzal
+     */
     @Override
     public boolean interact(IField field, IInventory inventory) {
+        //TODO: itt az inventory felesleges paraméter? Nem kéne ellenőrizni, hogy van-e broom feje a hókotrónak?
         ILayer current = field.getLayer();
         BroomClearLayerVisitor visitor = new BroomClearLayerVisitor();
         current.accept(visitor);
@@ -36,17 +51,29 @@ public class Broom extends Entity implements IHead, ShopItem{
         return true;
     }
 
+    /**
+     * @return Visszaadja az nevét.
+     */
     @Override
     public Item key() {
         return Item.BROOM;
     }
 
 
+    /**
+     * @return Visszaadja egy megvásárolható Broom árát.
+     */
     @Override
     public int price() {
         return Tracer.getInstance().askInt("Mennyibe kerül a Broom?");
     }
 
+    /**
+     * Alkalmazza a vásárlást.
+     * Jelzi az aktív hókotró inventory-jának, hogy adjon hozzá egy elemet a típusából.
+     * @param ctx visszaadja azt a hókotrót, amire történik a vásárlás
+     * @param amount a megvásárolni kívánt Broom-ok száma
+     */
     @Override
     public void apply(PurchaseContext ctx, int amount) {
         ISnowPlow sp = ctx.snowPlow();
--- app/skeleton/src/main/java/com/spring/app/skeleton/models/head/BroomClearLayerVisitor.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/head/BroomClearLayerVisitor.java
@@ -8,23 +8,43 @@ import com.spring.app.skeleton.models.layer.Layer;
 import com.spring.app.skeleton.models.layer.Snow;
 import com.spring.app.skeleton.utils.Entity;
 
+/**
+ * Eldönti, hogy a hányófej eltakaríthatja-e az adott mezőn az út felszínt.
+ */
 public class BroomClearLayerVisitor extends Entity implements ILayerVisitor{
+    /**
+     * Megadja, hogy a fej letakaríthatja-e az adott felületet.
+     */
     private boolean result;
 
     public boolean getResult(){
         return result;
     }
 
+    /**
+     * Eldönti, hogy a hányófej letakaríthatja-e a havat.
+     * El tudja takarítani minden esetben.
+     * @param s a hó amit el kéne takarítani
+     */
     @Override
     public void visit(Snow s) {
         result=true;
     }
 
+    /**
+     * Eldönti, hogy a hányófej letakaríthatja-e a jeget.
+     * Ha törött, akkor igen.
+     * @param i a jég amit el kéne takarítani
+     */
     @Override
     public void visit(Ice i) {
         result = i.getBroken();
     }
 
+    /**
+     * Eldönti, hogy a hányófej letakaríthatja-e a sima utat.
+     * Le tudja takarítani minden esetben.
+     */
     @Override
     public void visit(Layer l) {
         result = true;
