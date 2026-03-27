8623d7f011fd64018e095a421f5e896ea8cb5574
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-03-27 13:52:13 +0100

    comments

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/Vehicle.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/vehicle/Vehicle.java
@@ -3,7 +3,14 @@ package com.spring.app.skeleton.models.vehicle;
 import com.spring.app.skeleton.models.field.IField;
 import com.spring.app.skeleton.utils.Entity;
 
+/**
+ * Absztrakt jármű ősosztály.
+ * Megteszi a vezető által megadott lépéseket, belső működését menedzseli.
+ */
 public abstract class Vehicle extends Entity {
+    /**
+     * A jármű vezetője.
+     */
     IDriver driver;
 
     public Vehicle(IDriver driver){
@@ -12,12 +19,25 @@ public abstract class Vehicle extends Entity {
 
     abstract boolean canMove();
 
+    /**
+     * Ez a metódus felel a Vehicle összetöréséért. Hókotró nem törhet össze.
+     * @param v a másik jármű, amivel ez a jármű ütközik
+     */
     public abstract void contact(Vehicle v);
 
+    /**
+     * Ez a függvény végzi a játékos vagy autó lépését. Sima lépés, nincs megcsúszás, ezért false.
+     */
     public void step(){
         step(false);
     }
 
+    /**
+     * Ez a függvény végzi a Vehicle csúszás miatt történő kötelező 
+     * csúszását.
+     * @param forced true esetén a jármű kötelezően előre lép (getNext), false esetén a driver
+     * határozza meg a következő lépést (nextMove).
+     */
     public void step(boolean forced){
         if(!canMove()) return;
 
@@ -27,7 +47,17 @@ public abstract class Vehicle extends Entity {
         current.tryExit(next);
     }
 
+    /**
+     * A Vehicle mezővel történő interakcióját segíti.
+     * @param f a mező, amivel a jármű interakcióba kerül.
+     */
     public abstract void interact(IField f);
+
+    /**
+     * Lehetővé teszi a Visitor számára, hogy műveletet hajtson végre
+     * ezen a járművön. (IceSlipVisitor, SnowExitVisitor)
+     * @param visitor akinek a jármű engedélyezi a visit műveletet
+     */
     public abstract void accept(IVehicleVisitor visitor);
   
     public IDriver getDriver(){
