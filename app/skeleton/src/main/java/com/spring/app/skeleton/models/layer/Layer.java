678f48edb0bbafed826fccde03f33e0e33721bb5
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-03-27 15:39:51 +0100

    comments

--- app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/Layer.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/Layer.java
@@ -6,8 +6,15 @@ import com.spring.app.skeleton.models.random.IRandom;
 import com.spring.app.skeleton.models.vehicle.Vehicle;
 import com.spring.app.skeleton.utils.Entity;
 
+/**
+ * Aszfaltként interaktál a járművekkel.
+ */
 public class Layer extends Entity implements ILayer {
 
+    /**
+     * Ez a metódus határozza meg, hogy milyen réteg lesz az eredmény,
+     * két réteg egymásra helyezése után.
+     */
     @Override
     public ILayer merge(ILayer layer) {
         return layer;
@@ -18,16 +25,31 @@ public class Layer extends Entity implements ILayer {
         visitor.visit(this);
     }
 
+    /**
+     * Ez a függvény meghatározza a réteg alapján, 
+     * hogy a rálépett Vehicle megcsúszik-e vagy nem.
+     * A layer osztály esetében nem fog sose megcsúszni a jármű.
+     */
     @Override
     public boolean slip(Vehicle v, IRandom random) {
         return false;
     }
 
+    /**
+     * Meghatározza hogy az adott jármű ki tud-e lépni a Layerből. 
+     * Ebből mindenki ki tud lépni független a járműtől.
+     */
     @Override
     public boolean canExit(Vehicle v) {
         return true;
     }
 
+    /**
+     * A jármű belépésének a rétegre való hatását eredményezi.
+     * @return Nincs változás, visszatér saját magával.
+     * (Egy jármű más esetben ebben a metódusban taposná le, ha hó lenne.
+     * Aszfaltot nem lehet letaposni.)
+     */
     @Override
     public ILayer enter() {
         return this;
--- app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/Snow.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/models/layer/Snow.java
@@ -6,8 +6,13 @@ import com.spring.app.skeleton.models.random.IRandom;
 import com.spring.app.skeleton.models.vehicle.Vehicle;
 import com.spring.app.skeleton.utils.Entity;
 
-
+/**
+ * Hóréteg. Autók és buszok elakadhatnak benne.
+ */
 public class Snow extends Entity implements ILayer {
+    /**
+     * Megadja hogy a hó hány réteges.
+     */
     private int level;
 
     public Snow(int level){
@@ -22,6 +27,12 @@ public class Snow extends Entity implements ILayer {
         level = l;
     }
 
+    /**
+     * Ez a metódus határozza meg, hogy milyen réteg lesz az eredmény,
+     * két réteg egymásra helyezése után.
+     * @param a másik réteg
+     * @return az eredmény réteg
+     */
     @Override
     public ILayer merge(ILayer layer) {
         SnowMergeVisitor visitor = new SnowMergeVisitor(this);
@@ -29,16 +40,28 @@ public class Snow extends Entity implements ILayer {
         return visitor.getResult();
     }
 
+    /**
+     * Lehetővé teszi a visitor számára, hogy műveletet hajtson végre ezen a rétegen.
+     * @param visitor a réteget feldolgozó visitor objektum
+     */
     @Override
     public void accept(ILayerVisitor visitor) {
         visitor.visit(this);
     }
 
+    /**
+     * Ez a függvény meghatározza a réteg alapján, hogy a rálépett Vehicle megcsúszik-e vagy nem.
+     * @return A Snow osztály esetében nem tudnak megcsúszni a járművek.
+     */
     @Override
     public boolean slip(Vehicle v, IRandom random) {
         return false;
     }
 
+    /**
+     * @return Meghatározza hogy az adott jármű ki tud-e lépni a Layerből. 
+     * (Autó és busz magas hóban elakad, de a hókotrót nem akadályozza.)
+     */
     @Override
     public boolean canExit(Vehicle v) {
         SnowExitVisitor visitor = new SnowExitVisitor(this);
@@ -46,6 +69,10 @@ public class Snow extends Entity implements ILayer {
         return visitor.getResult();
     }
 
+    /**
+     * Az autók rálépésének hatására csökkenti a szintjét, ha a legalacsonyabb, akkor jéggé változik.
+     * @return az eredmény réteg
+     */
     @Override
     public ILayer enter() {
         if(level > 1){
