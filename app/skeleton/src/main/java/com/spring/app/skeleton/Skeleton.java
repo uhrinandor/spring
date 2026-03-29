dd8b66bb85d38e885585fe1da9ef457e498c053e
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-03-29 15:37:33 +0200

    bus_usecase

--- app/skeleton/src/main/java/com/spring/app/skeleton/Skeleton.java
+++ app/skeleton/src/main/java/com/spring/app/skeleton/Skeleton.java
@@ -38,6 +38,9 @@ public class Skeleton {
     public Skeleton() {
         menuItems.add(new MenuItem("Exit", this::exit));
         menuItems.add(new MenuItem("StepCarFromSnow", this::stepCarFromSnow));
+        menuItems.add(new MenuItem( "StepBusFromSnow", this::StepBusFromSnow));
+        menuItems.add(new MenuItem("StepBusToIce", this::StepBusToIce));
+        menuItems.add(new MenuItem("StepBusWreck", this::StepBusWreck));
     }
 
     /**
@@ -82,6 +85,41 @@ public class Skeleton {
         Tracer.show();
 
         car.step();
+        Entity.reset();
+    }
+
+    private void StepBusFromSnow(){
+        Tracer.hide();
+        IField start = getBus();
+        
+        Vehicle bus = start.getVehicle();
+        Tracer.show();
+
+        bus.step();
+        Entity.reset();
+    }
+
+    private void StepBusToIce(){
+        Tracer.hide();
+        IField start = getBus();
+        
+        IField field=start.getRight();
+        Vehicle bus = field.getVehicle();
+        Tracer.show();
+
+        bus.step();
+        Entity.reset();
+    }
+
+    private void StepBusWreck(){
+        Tracer.hide();
+        IField start = getBus();
+
+        IField field=start.getRight().getRight();
+        Vehicle bus = field.getVehicle();
+        Tracer.show();
+
+        bus.step();
         Entity.reset();
     }
 
@@ -206,13 +244,13 @@ public class Skeleton {
         PlayerDriver d2 = new PlayerDriver();
         PlayerDriver d3 = new PlayerDriver();
 
-        d1.setCurrent(f1);
-        d2.setCurrent(f3);
-        d3.setCurrent(f6);
-
         d1.setNext(f2);
         d2.setNext(f4);
         d3.setNext(f7);
+
+        d1.setCurrent(f1);
+        d2.setCurrent(f3);
+        d3.setCurrent(f6);
 
         Station s1 = new Station(f2);
         Station s2 = new Station(f6);
