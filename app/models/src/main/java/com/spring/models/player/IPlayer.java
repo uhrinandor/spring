897f13eb3a3b5221bd56f958dd34e5999ce36e86
BernathTamas7 <bernathtamasx@gmail.com>
2026-04-23 19:58:17 +0200

    merge conflict

--- app/models/src/main/java/com/spring/models/player/IPlayer.java
+++ app/models/src/main/java/com/spring/models/player/IPlayer.java
@@ -8,7 +8,7 @@ import com.spring.models.vehicle.Vehicle;
  * Ezen interfész alatt kezeljük a játékosainkat. Ezen kívül, ez az interfész kezeli a játékosok 
  * pontjait.
  */ 
-public interface IPlayer extends IRPlayer{
+public interface IPlayer extends IRPlayer, ICollector{
     /**
      * @param amount felveszünk egy adott mennyiségű jutalmat.
      * (A SnowPlowPlayer pénzt gyűjt (money), a BusPlayer pontokat gyűjt (point).)
