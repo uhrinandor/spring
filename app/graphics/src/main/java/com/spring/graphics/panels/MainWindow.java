67e22b21450e43e47ab729c08ecb333405838d40
Uhrin Andor <andoruhrin@gmail.com>
2026-05-20 15:13:22 +0200

    mainwindow navigation

--- app/graphics/src/main/java/com/spring/graphics/panels/MainWindow.java
+++ app/graphics/src/main/java/com/spring/graphics/panels/MainWindow.java
@@ -1,56 +1,49 @@
 package com.spring.graphics.panels;
 import java.awt.BorderLayout;
-import java.awt.Point;
-import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
 
+import com.spring.controllers.controllers.BusPlayerController;
 import com.spring.controllers.controllers.CycleController;
+import com.spring.controllers.controllers.EndController;
 import com.spring.controllers.controllers.InitController;
 import com.spring.controllers.controllers.SnowPlowController;
 import com.spring.controllers.controllers.SnowPlowPlayerController;
+import com.spring.controllers.listeners.CycleListener;
+import com.spring.controllers.listeners.EndListener;
+import com.spring.controllers.listeners.InitListener;
+import com.spring.controllers.listeners.OnErrorListener;
+import com.spring.controllers.listeners.SnowPlowListener;
+import com.spring.controllers.listeners.SnowPlowPlayerListener;
 import com.spring.controllers.utils.GameContext;
 import com.spring.graphics.panels.map.MapGenerator;
 import com.spring.graphics.panels.map.MapPanel;
-import com.spring.graphics.panels.map.interfaces.IMap;
+import com.spring.graphics.panels.menubars.BusPlayerView;
+import com.spring.graphics.panels.menubars.EndView;
 import com.spring.graphics.panels.menubars.InitView;
+import com.spring.graphics.panels.menubars.MenuBar;
 import com.spring.graphics.panels.menubars.SnowplowPlayerView;
 import com.spring.graphics.panels.menubars.SnowplowView;
-import com.spring.graphics.panels.menubars.MenuBar;
-import com.spring.models.field.IRField;
-import com.spring.models.layer.Layer;
+import com.spring.models.buildings.Building;
+import com.spring.models.player.BusPlayer;
 import com.spring.models.player.SnowplowPlayer;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements InitListener, CycleListener, OnErrorListener, SnowPlowListener, SnowPlowPlayerListener, EndListener {
+    MapPanel mapPanel;
+    MenuBar menuBar;
+    CycleController cycleController;
+    SnowPlowPlayerController snowPlowPlayerController;
+    
     public MainWindow() {
         setTitle("Spring");
         setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
 
-        GameContext context = new GameContext();
-        InitController initController = new InitController(context);
-        CycleController cycleController = new CycleController(context);
-        SnowPlowController snowPlowController = new SnowPlowController();
-        snowPlowController.setPlayer(new SnowplowPlayer());
-        SnowPlowPlayerController snowPlowPlayerController = new SnowPlowPlayerController(cycleController, null);
-
-        MapPanel mapPanel = new MapPanel(context);
-        InitView initView = new InitView(mapPanel, initController);
-        SnowplowPlayerView snowplowPlayerView = new SnowplowPlayerView(snowPlowPlayerController);
-        SnowplowView snowplowView = new SnowplowView(snowPlowController);
-
-        MenuBar menuBar = new MenuBar();
-        setLayout(new BorderLayout());
-
-        menuBar.change(snowplowView);
-        add(menuBar, BorderLayout.NORTH);
-        add(mapPanel, BorderLayout.CENTER);
-
-        chooseAndGenerateMap(initController);
-        renderBase(context, mapPanel);
+        onNewGame();        
 
         setVisible(true);
     }
@@ -79,21 +72,81 @@ public class MainWindow extends JFrame {
         }
     }
 
-    public void genMap(InitController initController) {
-        initController.addField(new Layer(), false);
-        initController.addField(new Layer(), false);
-        initController.addField(new Layer(), false);
-        initController.addField(new Layer(), false);
-        initController.setSide(true, 0, 1);
-        initController.setFrontField(1, 2);
-        initController.setFrontField(2, 3);
+    @Override
+    public void onSnowPlowSelected() {
+        SnowPlowController snowPlowController = new SnowPlowController();
+        snowPlowController.setPlayer(snowPlowPlayerController.info());
+        snowPlowController.addErrorListener(this);
+        snowPlowController.addSnowPlowListener(this);
+        SnowplowView snowplowView = new SnowplowView(snowPlowController);
+        menuBar.change(snowplowView);
     }
 
-    public void renderBase(GameContext context, IMap map) {
-        for(IRField field : context.getFields()) {
-            int x = new Random().nextInt(1, 400);
-            int y = new Random().nextInt(1, 400);
-            map.addField(field, new Point(x, y));
-        }
+    @Override
+    public void onSnowplowClosed() {
+        SnowplowPlayerView view = new SnowplowPlayerView(snowPlowPlayerController);
+        menuBar.change(view);
+    }
+
+    @Override
+    public void onError(String message) {
+        JOptionPane.showMessageDialog(this, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    @Override
+    public void nextSnowPlowPlayer(SnowplowPlayer player) {
+        snowPlowPlayerController = new SnowPlowPlayerController(cycleController, player);
+        snowPlowPlayerController.addErrorListener(this);
+        snowPlowPlayerController.addSnowPlowPlayerListener(this);
+        SnowplowPlayerView view = new SnowplowPlayerView(snowPlowPlayerController);
+        menuBar.change(view);
+    }
+
+    @Override
+    public void nextBusPlayer(BusPlayer player) {
+        BusPlayerController busPlayerController = new BusPlayerController(cycleController);
+        busPlayerController.addErrorListener(this);
+        BusPlayerView busPlayerView = new BusPlayerView(busPlayerController);
+        menuBar.change(busPlayerView);
+    }
+
+    @Override
+    public void onGameEnd(SnowplowPlayer winner1, BusPlayer winner2) {
+        EndController endController = new EndController(winner1, winner2, cycleController.getContext());
+        endController.addEndListener(this);
+        EndView endView = new EndView(endController);
+        menuBar.change(endView);
+    }
+
+    @Override
+    public void onCarPlaced(Building building) {
+        JOptionPane.showMessageDialog(this, String.format("A new car has been placed next to %s", building.toString()), "New Car", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void onGameStarted(GameContext context) {
+        this.cycleController = new CycleController(context);
+        cycleController.addCycleListener(this);
+        cycleController.addCycleListener(this);
+    }
+
+    @Override
+    public void onNewGame() {
+        getContentPane().removeAll();
+        GameContext context = new GameContext();
+        InitController initController = new InitController(context);
+        this.mapPanel = new MapPanel(context);
+        InitView initView = new InitView(mapPanel, initController);
+
+        this.menuBar = new MenuBar();
+        setLayout(new BorderLayout());
+
+        menuBar.change(initView);
+        add(menuBar, BorderLayout.NORTH);
+        add(mapPanel, BorderLayout.CENTER);
+
+        chooseAndGenerateMap(initController);
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
 }
--- app/graphics/src/main/java/com/spring/graphics/panels/map/MapPanel.java
+++ app/graphics/src/main/java/com/spring/graphics/panels/map/MapPanel.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
