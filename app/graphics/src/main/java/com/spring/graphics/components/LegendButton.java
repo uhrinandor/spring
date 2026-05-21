a90aa20b9faad912b4a6cb7ed330c0765092ac29
Uhrin Andor <andoruhrin@gmail.com>
2026-05-21 22:41:58 +0200

    bus.

--- app/graphics/src/main/java/com/spring/graphics/components/LegendButton.java
+++ app/graphics/src/main/java/com/spring/graphics/components/LegendButton.java
@@ -3,8 +3,12 @@ package com.spring.graphics.components;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LegendButton extends BaseButton{
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 
+public class LegendButton extends BaseButton{
+    int counter = 0;
     Legend legend;
 
     public LegendButton(){
@@ -15,7 +19,28 @@ public class LegendButton extends BaseButton{
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent E){
+                counter++;
+                if(counter >= 10){
+                    JDialog dialog = new JDialog((JDialog) null, "Plot Twist", true);
+                    dialog.setSize(300, 300);
+                    dialog.setLocationRelativeTo(null);
+
+                    try {
+                        ImageIcon image = new ImageIcon(
+                            getClass().getResource("/logo/bus.jpeg")
+                        );
+
+                        JLabel label = new JLabel(image);
+                        dialog.add(label);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    dialog.setVisible(true);
+                }
                 openLegend();
+                
             }
         });
     }
