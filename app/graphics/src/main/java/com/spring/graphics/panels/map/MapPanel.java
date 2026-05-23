e4c0ad30ecf2751d58e146483bba490d7a7dcf24
Turai Johanna <231061004+turaij@users.noreply.github.com>
2026-05-23 13:43:02 +0200

    map3_coordinates

--- app/graphics/src/main/java/com/spring/graphics/panels/map/MapPanel.java
+++ app/graphics/src/main/java/com/spring/graphics/panels/map/MapPanel.java
@@ -84,7 +84,13 @@ public class MapPanel extends JPanel implements IMap, RoadViewListener {
         for(FieldView from : fieldViews){
             boolean isCrossRoad = false;
             IRoad front = from.getField().getFront(); 
-            List<IField> available = new ArrayList<IField>(front.getAvailable());
+            List<IField> available;
+            if (front==null){
+                available=new ArrayList<IField>();
+            }
+            else{
+                available=new ArrayList<IField>(front.getAvailable());
+            }
             IRField right = from.getField().getRight();
             for(CrossRoadView to : crossRoadViews){
                 if(to.getCrossRoad()==front){
